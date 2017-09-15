ccd7164765cbd7d8d821ae02eac672e0f70e5f55
Yael_Amaya <yiap2610@gmail.com>
2017-09-15 12:12:05 -0600

    Created toString method to describe monster

--- src/monster/model/MarshmallowMonster.java
+++ src/monster/model/MarshmallowMonster.java
@@ -24,4 +24,12 @@ public class MarshmallowMonster
 		this.tentacleAmount = tentacleAmount;
 		this.hasBloop = hasBloop;
 	}
+	
+	public String toString()
+	{
+		String description = "I am a monster, my name is " + name + "I have " + eyeCount + "eyes and only" ;
+		description +=  tentacleAmount + "tentacles but I have " + armCount + " arms!." + hasBloop + " tells you";
+		description += " if I have a bloop";
+		return description;
+	}
 }
