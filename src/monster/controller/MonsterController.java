acfc2c96bf5c80f135737cd00475ec8f01c68743
Yael_Amaya <yiap2610@gmail.com>
2017-09-15 13:50:44 -0600

    Now can interact with Jerry, and eat him

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,6 +1,7 @@
 package monster.controller;
 
 import monster.model.MarshmallowMonster;
+import java.util.Scanner;
 
 public class MonsterController
 {
@@ -9,7 +10,31 @@ public class MonsterController
 	{
 		MarshmallowMonster basic = new MarshmallowMonster();
 		System.out.println(basic);
-		MarshmallowMonster fred = new MarshmallowMonster("Silly Fred Monster", 7, 3, 6.7, true);
-		System.out.println(fred);
+		MarshmallowMonster Jerry = new MarshmallowMonster("Undead Marshmallow Jerry Pawn Zombie", 7, 4, 0.7, true);
+		System.out.println(Jerry);
+		System.out.println("I'm feeling hungry, I'm going to eat you Jerry... despite the fact you're a dead rotten corpse... MMM I CAN TASTE THE CALORIES, SO DELICIOUS!!!");
+		Jerry.setEyeCount(Jerry.getEyeCount() - 1);
+		System.out.println("Why would you do this!? I only have " + Jerry.getEyeCount() + " eyes now!!!");
+		
+		interactWithMonster(Jerry);
+		
+	}
+	
+	private void interactWithMonster(MarshmallowMonster currentMonster)
+	{
+		Scanner myScanner = new Scanner(System.in);
+		System.out.println(currentMonster.getName() + " Says: How many of my eyes will you eat!? Geez!!! (write number)");
+		int consumed = myScanner.nextInt();
+		currentMonster.setEyeCount(currentMonster.getEyeCount() - consumed);
+		System.out.println(currentMonster.getName() + " Says: I only have " + currentMonster.getEyeCount() + " eyes now... ouch, it hurts so much!!! Please don't take anything else!!!");
+		System.out.println("Bye Jerry. *nom* *nom* nom* *nom* *nom*");
+		currentMonster.setEyeCount(0);
+		currentMonster.setArmCount(0);
+		currentMonster.setTentacleAmount(0.0);
+		currentMonster.setBloop(false);
+		System.out.println(currentMonster.getName() + " is now completely eaten");
+		
+		
+		myScanner.close();
 	}
 }
--- src/monster/model/MarshmallowMonster.java
+++ src/monster/model/MarshmallowMonster.java
@@ -25,11 +25,62 @@ public class MarshmallowMonster
 		this.hasBloop = hasBloop;
 	}
 	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setEyeCount(int eyeCount)
+	{
+		this.eyeCount = eyeCount;
+	}
+	
+	public int getEyeCount()
+	{
+		return eyeCount;
+	}
+	
+	public void setArmCount(int armCount)
+	{
+		this.armCount = armCount;
+	}
+	
+	public int getArmCount()
+	{
+		return armCount;
+	}
+	
+	public void setTentacleAmount(double tentacleAmount)
+	{
+		this.tentacleAmount = tentacleAmount;
+	}
+	
+	public double getTentacleAmount()
+	{
+		return tentacleAmount;
+	}
+	
+	public void setBloop(boolean hasBloop)
+	{
+		this.hasBloop = hasBloop;
+	}
+	
+	public boolean getBloop()
+	{
+		return hasBloop;
+	}
+	
 	public String toString()
 	{
-		String description = "I am a monster, my name is " + name + "I have " + eyeCount + "eyes and only " ;
-		description +=  tentacleAmount + "tentacles, but I have " + armCount + "arms! " + hasBloop + " tells you";
-		description += " if I have a bloop";
+		String description = "I am a monster, my name is " + name + " I have " + eyeCount + " eyes and only " ;
+		description +=  tentacleAmount + " tentacles, but I have " + armCount + " arms! It is also " + hasBloop + " I have a bloop";
+		description += " My name is " + name + " because I was a pawn in chess and was named by my king, Calvin Harris.";
+		description += " I was slain in battle by the opposing King Yael Amaya and have come back to serve my King once more.";
 		return description;
 	}
 }
